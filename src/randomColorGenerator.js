d1d17b0525ad4e380073f701f8beac99cb0dee82
danwycliff <danwycliff6769@gmail.com>
2023-08-19 13:05:31 +0300

    codes

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -1,5 +1,5 @@
 // Nothing in this file needs to be altered (but it is your solution so feel free to!)
-export function getRandomColor() {
+function getRandomColor() {
   // this function generates a random hex color from the letters below
   const letters = "123456789AB"; // <-- cutting off top end so we get lighter colors
   let color = "#";
@@ -8,3 +8,4 @@ export function getRandomColor() {
   }
   return color;
 }
+export default getRandomColor;
